fb7b803a599f36e50acce8155b4e2739ac416620
RiccardoCataldi <riccardo.cataldi.1995@gmail.com>
2025-09-11 16:58:02 +0200

    Add terminal color support and update TokyoNight plugin configuration with priority and colorscheme initialization.

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- Set leader key to space
 vim.g.mapleader = " "
 
+vim.opt.termguicolors = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
