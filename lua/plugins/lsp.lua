c0e703565c9c783a90c30b7c63b71a049a9bcf66
RiccardoCataldi <riccardo.cataldi.1995@gmail.com>
2025-09-11 10:25:23 +0200

    add LSP and Treesitter configurations for enhanced development support

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,46 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "pyright" }  -- Python LSP server
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            -- Python LSP configuration
+            lspconfig.pyright.setup({
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = "basic",
+                            autoSearchPaths = true,
+                            useLibraryCodeForTypes = true
+                        }
+                    }
+                }
+            })
+
+            -- Global LSP keymaps
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+            vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, {})
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+        end
+    }
+}
